11947f79e1715400d90f4b88d5426200be581977
eun <gst8537zz@gmail.com>
2025-10-13 12:08:54 +0900

    jwt 인터셉터 추가

---
+++ src/main/java/com/kedu/newboard/interceptors/JWTInterceptor.java
@@ -0,0 +1,36 @@
+package com.kedu.newboard.interceptors;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.kedu.newboard.utils.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class JWTInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JWTUtil jwt;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 나중엔 url 별로 어떤 url은 인터셉트하고 어떤 url은 인터셉트 안 할건지 지정
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+
+        try {
+            DecodedJWT djwt = jwt.verifyToken(token);
+            // 인터셉터에서 미리 ID를 꺼내서 넘겨주는게 편함. - 가장 많이 사용하는 정보
+            request.setAttribute("loginId", djwt.getSubject());
+            return true;
+        }catch (Exception e){ // 인증 실패시
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Error");
+            return false;
+        }
+
+    }
+}
